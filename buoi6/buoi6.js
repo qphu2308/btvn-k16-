70c3f545dcdf975f44cbccc8a86617154a9db216
Quang Phu <phule75210@gmail.com>
2023-09-29 01:37:29 +0700

    btvn

--- buoi6/buoi6.js
+++ buoi6/buoi6.js
@@ -1,1 +1,11 @@
-aaa;
+//bai4
+let bangChuCai = ["a", "b", "c", "d", "e"];
+let daySo = [5, 6, 7, 8];
+//bangChuCai.splice(1, 2, "f", "g");
+//bangChuCai.unshift("1", "2", "3");
+//let x = bangChuCai.pop();
+//console.log(bangChuCai);
+//let stt = bangChuCai.concat(daySo);
+//bangChuCai.reverse();
+let soPhanTuCuaMang = daySo.length;
+console.log(soPhanTuCuaMang);
