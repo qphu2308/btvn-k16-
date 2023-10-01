d296dce5db9d92d75df3b300a80af3dba97449d3
Quang Phu <phule75210@gmail.com>
2023-10-01 18:24:42 +0700

    btvn buoi 7

---
+++ buoi7/buoi7.js
@@ -0,0 +1,20 @@
+//bai1
+{
+  //  var age = 18;
+}
+{
+  //  let age = 19;
+}
+//console.log(age);
+// khi console.log(age) thi se duoc 18 vi var la pham vi ham
+
+//bai2
+function f1() {
+  var age = 18;
+  console.log(age);
+}
+function f2() {
+  let age = 19;
+}
+console.log(age);
+// khi console.log(age) thi se bi lỗi vi khong co goi f1, f2
